78f38eb474ff474934bad3c52e3f792918985f2a
Tunç <tunccindas7570@gmail.com>
2024-07-25 14:10:56 +0300

    up

--- Core/Dentist.Application/Features/Mediator/Handlers/AuthorizationHandlers/CreateNewPasswordQueryHandler.cs
+++ Core/Dentist.Application/Features/Mediator/Handlers/AuthorizationHandlers/CreateNewPasswordQueryHandler.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Dentist.Application.Features.Mediator.Queries.AuthorizationQueries;
+using Dentist.Application.Features.Mediator.Results.AuthorizationResults;
+using Dentist.Application.Interfaces;
+using Dentist.Application.Tools;
+using Dentist.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,41 @@ using System.Threading.Tasks;
 
 namespace Dentist.Application.Features.Mediator.Handlers.AuthorizationHandlers
 {
-    internal class CreateNewPasswordQueryHandler
-    {
-    }
+    public class CreateNewPasswordQueryHandler : IRequestHandler<CreateNewPasswordQuery, CreateNewPasswordQueryResult>
+	{
+		private readonly IRepository<User> _repository;
+		private readonly int _iteration = 6;
+		private readonly string _pepper = "XslMeQuwL2cqoksVkISEAlb26suddECzwGvBYUf4JmdkCmYl";
+
+		public CreateNewPasswordQueryHandler(IRepository<User> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<CreateNewPasswordQueryResult> Handle(CreateNewPasswordQuery request, CancellationToken cancellationToken)
+		{
+			var message = "";
+			var newpassword = "";
+			var user = await _repository.GetByFilterAsync(x => x.UserName == request.Username);
+			if (user != null)
+			{
+				message = "Success";
+				var newpasswordsalt = PasswordHasher.GenerateSalt();
+				newpassword = PasswordGenerator.GeneratePassword();
+				var passwordHash = PasswordHasher.ComputeHash(newpassword, newpasswordsalt, _pepper, _iteration);
+
+				user.PasswordSalt = newpasswordsalt;
+				user.PasswordHash = passwordHash;
+				await _repository.UpdateAsync(user);
+
+			}
+			else
+				message = "Böyle bir kullanıcı bulunamadı.";
+			return new CreateNewPasswordQueryResult
+			{
+				NewPassword = newpassword,
+				Message = message
+			};
+		}
+	}
 }
--- Core/Dentist.Application/Features/Mediator/Handlers/AuthorizationHandlers/ResetPasswordQueryHandler.cs
+++ Core/Dentist.Application/Features/Mediator/Handlers/AuthorizationHandlers/ResetPasswordQueryHandler.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Dentist.Application.Features.Mediator.Queries.AuthorizationQueries;
+using Dentist.Application.Features.Mediator.Results.AuthorizationResults;
+using Dentist.Application.Interfaces;
+using Dentist.Application.Tools;
+using Dentist.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,41 @@ using System.Threading.Tasks;
 
 namespace Dentist.Application.Features.Mediator.Handlers.AuthorizationHandlers
 {
-    internal class ResetPasswordQueryHandler
-    {
-    }
+	public class ResetPasswordQueryHandler : IRequestHandler<ResetPasswordQuery, ResetPasswordQueryResult>
+	{
+		private readonly IRepository<User> _repository;
+		private readonly int _iteration = 6;
+		private readonly string _pepper = "XslMeQuwL2cqoksVkISEAlb26suddECzwGvBYUf4JmdkCmYl";
+
+		public ResetPasswordQueryHandler(IRepository<User> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<ResetPasswordQueryResult> Handle(ResetPasswordQuery request, CancellationToken cancellationToken)
+		{
+			ResetPasswordQueryResult result = new();
+			var user = await _repository.GetByFilterAsync(x => x.UserName == request.Username);
+			if (user != null)
+			{
+
+				var passwordHash = PasswordHasher.ComputeHash(request.OldPassword, user.PasswordSalt, _pepper, _iteration);
+				if (passwordHash == user.PasswordHash)
+				{
+					var newpasswordsalt = PasswordHasher.GenerateSalt();
+					user.PasswordSalt = newpasswordsalt;
+					user.PasswordHash = PasswordHasher.ComputeHash(request.NewPassword, newpasswordsalt, _pepper, _iteration);
+					await _repository.UpdateAsync(user);
+					result.Message = "Şifre başarıyla değiştirildi.";
+				}
+				else
+					result.Message = "Eski şifre hatalı";
+
+			}
+			else
+				result.Message = "Böyle bir kullanıcı bulunamadı";
+
+			return result;
+		}
+	}
 }
