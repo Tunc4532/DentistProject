a574b0c7f5648783ec132f0e1819f8f6439aa046
Tunç <tunccindas7570@gmail.com>
2024-08-08 09:45:40 +0300

    update 8082024

--- Frontends/Dentist.WebUI/ViewComponents/HomeComponents/_HomeSliderCardComponentPartial.cs
+++ Frontends/Dentist.WebUI/ViewComponents/HomeComponents/_HomeSliderCardComponentPartial.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dentist.Dtos.SliderCardDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Dentist.WebUI.ViewComponents.HomeComponents
 {
 	public class _HomeSliderCardComponentPartial : ViewComponent
 	{
-		public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<_HomeSliderCardComponentPartial> _logger;
+
+        public _HomeSliderCardComponentPartial(IHttpClientFactory httpClientFactory, ILogger<_HomeSliderCardComponentPartial> logger)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
 		{
-			return View();
-		}
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7119/api/SliderCards");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSliderCardDto>>(jsonData);
+                return View(values);
+            }
+            _logger.LogError("Null reference exception occurred in SliderCardList method.");
+
+            return View();
+        }
 	}
 }
